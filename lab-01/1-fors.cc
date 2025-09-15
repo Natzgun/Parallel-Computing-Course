41baf5993173e4a8082d1cb0b60c84dd6a580305
Natzgun <itzlilouxx@gmail.com>
2025-09-14 22:10:27 -0500

    Compared twice fors, first based in cache line (contiguous) and second based in columns (no contiguous)

--- lab-01/1-fors.cc
+++ lab-01/1-fors.cc
@@ -1,22 +1,41 @@
+#include <algorithm>
+#include <chrono>
 #include <iostream>
+#include <vector>
 
-#define MAX 1000
+using std::cout;
+using std::endl;
+using std::vector;
+
+#define vvi vector<vector<int>>
+#define vi vector<int>
+
+#define MAX 20000
 
 int main (int argc, char *argv[]) {
 
-  double A[MAX][MAX], x[MAX], y[MAX];
-
   // Initializae A and x, assign y = 0
+  vvi A(MAX, vi(MAX, 1));
+  vi x(MAX, 1);
+  vi y(MAX, 0);
 
   // First pair of loops
+  auto t1 = std::chrono::high_resolution_clock::now();
   for (int i = 0; i < MAX; i++)
     for (int j = 0; j < MAX; j++)
       y[i] += A[i][j] * x[j];
+  auto t2 = std::chrono::high_resolution_clock::now();
+  cout << "First matrix: " << std::chrono::duration<double>(t2 - t1).count() << endl;
 
   // Assign y = 0
+  std::fill(y.begin(), y.end(), 0);
+  
+  t1 = std::chrono::high_resolution_clock::now();
   for (int j = 0; j < MAX; j++)
     for (int i = 0; i < MAX; i++)
       y[i] += A[i][j] * x[j];
+  t2 = std::chrono::high_resolution_clock::now();
+  cout << "Second matrix: " << std::chrono::duration<double>(t2 - t1).count() << endl;
 
   return 0;
 }
