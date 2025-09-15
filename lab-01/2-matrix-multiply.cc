8a9d7d9a28bcfbf30f962ed5aa38a34ef5ff2db0
Natzgun <itzlilouxx@gmail.com>
2025-09-15 10:58:19 -0500

    Comment lines for use valgrind

--- lab-01/2-matrix-multiply.cc
+++ lab-01/2-matrix-multiply.cc
@@ -30,9 +30,9 @@ vvi multiplyBlocks(const vvi &a, const vvi &b, int blockSize) {
     for (int jj = 0; jj < m; jj += blockSize)
       for (int kk = 0; kk < p; kk += blockSize)
 
-        for (int i = ii; i < std::min(ii + blockSize, n); i++)
-          for (int j = jj; j < std::min(jj + blockSize, m); j++)
-            for (int k = kk; k < std::min(kk + blockSize, p); k++)
+        for (int i = ii; i < ii + blockSize; i++)
+          for (int j = jj; j < jj + blockSize; j++)
+            for (int k = kk; k < kk + blockSize; k++)
               result[i][j] += a[i][k] * b[k][j];
   return result;
 }
@@ -50,49 +50,49 @@ int main() {
     vvi A10(10, vi(10, 1));
     vvi B10(10, vi(10, 2));
 
-    auto t1 = std::chrono::high_resolution_clock::now();
+    // auto t1 = std::chrono::high_resolution_clock::now();
     vvi C10 = multiply(A10, B10);
-    auto t2 = std::chrono::high_resolution_clock::now();
-    cout << "clásica 10x10: "
-         << std::chrono::duration<double>(t2 - t1).count() << " segundos" << endl;
+    // auto t2 = std::chrono::high_resolution_clock::now();
+    // cout << "clásica 10x10: "
+    //      << std::chrono::duration<double>(t2 - t1).count() << " segundos" << endl;
 
-    t1 = std::chrono::high_resolution_clock::now();
+    // t1 = std::chrono::high_resolution_clock::now();
     vvi C10b = multiplyBlocks(A10, B10, 2);
-    t2 = std::chrono::high_resolution_clock::now();
-    cout << "bloques 10x10: "
-         << std::chrono::duration<double>(t2 - t1).count() << " segundos" << endl;
+    // t2 = std::chrono::high_resolution_clock::now();
+    // cout << "bloques 10x10: "
+         // << std::chrono::duration<double>(t2 - t1).count() << " segundos" << endl;
 
 
-    vvi A100(100, vi(100, 1));
-    vvi B100(100, vi(100, 2));
+    // vvi A100(100, vi(100, 1));
+    // vvi B100(100, vi(100, 2));
+    //
+    // t1 = std::chrono::high_resolution_clock::now();
+    // vvi C100 = multiply(A100, B100);
+    // t2 = std::chrono::high_resolution_clock::now();
+    // cout << "clásica 100x100: "
+    //      << std::chrono::duration<double>(t2 - t1).count() << " segundos" << endl;
+    //
+    // t1 = std::chrono::high_resolution_clock::now();
+    // vvi C100b = multiplyBlocks(A100, B100, 10);
+    // t2 = std::chrono::high_resolution_clock::now();
+    // cout << "bloques 100x100: "
+    //      << std::chrono::duration<double>(t2 - t1).count() << " segundos" << endl;
 
-    t1 = std::chrono::high_resolution_clock::now();
-    vvi C100 = multiply(A100, B100);
-    t2 = std::chrono::high_resolution_clock::now();
-    cout << "clásica 100x100: "
-         << std::chrono::duration<double>(t2 - t1).count() << " segundos" << endl;
 
-    t1 = std::chrono::high_resolution_clock::now();
-    vvi C100b = multiplyBlocks(A100, B100, 10);
-    t2 = std::chrono::high_resolution_clock::now();
-    cout << "bloques 100x100: "
-         << std::chrono::duration<double>(t2 - t1).count() << " segundos" << endl;
-
-
-    vvi A1000(1000, vi(1000, 1));
-    vvi B1000(1000, vi(1000, 2));
-
-    t1 = std::chrono::high_resolution_clock::now();
-    vvi C1000 = multiply(A1000, B1000);
-    t2 = std::chrono::high_resolution_clock::now();
-    cout << "clásica 1000x1000: "
-         << std::chrono::duration<double>(t2 - t1).count() << " segundos" << endl;
-
-    t1 = std::chrono::high_resolution_clock::now();
-    vvi C1000b = multiplyBlocks(A1000, B1000, 50);
-    t2 = std::chrono::high_resolution_clock::now();
-    cout << "bloques 1000x1000: "
-         << std::chrono::duration<double>(t2 - t1).count() << " segundos" << endl;
+    // vvi A1000(1000, vi(1000, 1));
+    // vvi B1000(1000, vi(1000, 2));
+    //
+    // t1 = std::chrono::high_resolution_clock::now();
+    // vvi C1000 = multiply(A1000, B1000);
+    // t2 = std::chrono::high_resolution_clock::now();
+    // cout << "clásica 1000x1000: "
+    //      << std::chrono::duration<double>(t2 - t1).count() << " segundos" << endl;
+    //
+    // t1 = std::chrono::high_resolution_clock::now();
+    // vvi C1000b = multiplyBlocks(A1000, B1000, 50);
+    // t2 = std::chrono::high_resolution_clock::now();
+    // cout << "bloques 1000x1000: "
+    //      << std::chrono::duration<double>(t2 - t1).count() << " segundos" << endl;
 
     return 0;
 }
